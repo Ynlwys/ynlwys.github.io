21abe1d6ec4dd57d4270047aa3972b22abb2e37b
Ynlwys <Ynlwys@Gmail.com>
2021-01-05 14:30:18 +0800

    Update pagic.config.ts

--- pagic.config.ts
+++ pagic.config.ts
@@ -1,58 +1,46 @@
-// import { React } from 'https://deno.land/x/pagic/mod.ts';
-
 export default {
-  srcDir: '.',
-  exclude: ['LICENSE'],
-  root: '/pagic_template_docs/',
-
-  theme: 'docs',
-  plugins: ['sidebar', 'prev_next'],
-  title: 'Pagic template docs',
-  description: 'Use this template to create a Pagic site with the docs theme',
-  // To use jsx syntax, please rename this file to pagic.config.tsx
-  // head: <>
-  //   <link rel="icon" type="image/png" href="/favicon.png" />
-  //   <script src="/assets/custom.js" />
-  // </>,
+  theme: 'https://raw.githubusercontent.com/ynlwys/pagic/master/src/themes/blog/mod.ts',
+  plugins: ['blog'],
+  title: 'YNlwys的博客',
+  description: '欢迎来到我的博客，这里搜集了我的技术文章和生活感悟，欢迎一起交流成长。',
+  blog: {
+    root: '/posts/',
+    social: {
+      github: 'Ynlwys',
+      email: 'Ynlwys@gmail.com',
+      twitter: 'Ynlwys',
+    },
+  },
   nav: [
     {
-      text: 'Docs',
-      link: '/pagic_template_docs/introduction/index.html',
+      text: '首页',
+      link: '/index.html',
+      icon: 'czs-home-l',
     },
     {
-      text: 'Pagic',
-      link: 'https://pagic.org/',
+      text: '分类',
+      link: '/categories/index.html',
+      icon: 'czs-category-l',
     },
     {
-      text: 'About',
-      link: '/pagic_template_docs/about/index.html',
-      align: 'right',
+      text: '标签',
+      link: '/tags/index.html',
+      icon: 'czs-tag-l',
+    },
+    {
+      text: '关于',
+      link: '/about/index.html',
+      icon: 'czs-about-l',
+    },
+    {
+      text: '归档',
+      link: '/archives/index.html',
+      icon: 'czs-box-l',
+    },
+    {
+      text: '友情链接',
+      link: '/links/index.html',
+      icon: 'czs-link-l',
     },
   ],
-  github: 'https://github.com/xcatliu/pagic_template_docs',
-  sidebar: {
-    '/': [
-      'introduction/README.md',
-      {
-        link: 'test_pages/README.md',
-        children: ['test_pages/markdown_test.md', 'test_pages/front_matter.md', 'test_pages/react_hooks_test.tsx'],
-      },
-      {
-        text: 'Folder',
-        children: [
-          'folder/foo.md',
-          {
-            text: 'Custom sidebar text',
-            link: 'folder/bar.md'
-          }
-        ]
-      }
-    ],
-  },
-  tools: {
-    editOnGitHub: true,
-    backToTop: true,
-  },
-
-  port: 8000,
 };
