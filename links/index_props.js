0585211039b9f355b97fc481c34bbb3b1dff377f
Ynlwys <Ynlwys@users.noreply.github.com>
2021-01-06 03:24:12 +0000

    deploy: 345e4e053367e5c823b6ad48d3c69aa67c7669a0

--- links/index_props.js
+++ links/index_props.js
@@ -1,9 +1,9 @@
 import projectConfig from '/pagic.config.js';
 export default {
     config: { "root": "/", ...projectConfig, branch: 'main' },
-    'pagePath': "README.md",
+    'pagePath': "links/README.md",
     'layoutPath': "_layout.tsx",
-    'outputPath': "index.html",
+    'outputPath': "links/index.html",
     'title': "Pagic template docs",
     'content': React.createElement("article", { dangerouslySetInnerHTML: {
             __html: '<h1>Pagic template docs</h1>\n<h2 id="1-click-the-use-this-template-button-in-this-repo">1. Click the <em>Use this template</em> button in <a href="https://github.com/xcatliu/pagic_template_docs">this repo</a><a class="anchor" href="#1-click-the-use-this-template-button-in-this-repo">§</a></h2>\n<h2 id="2-enter-the-repository-name-and-click-the-create-repository-from-template-button">2. Enter the <em>Repository name</em> and click the <em>Create repository from template</em> button<a class="anchor" href="#2-enter-the-repository-name-and-click-the-create-repository-from-template-button">§</a></h2>\n<h2 id="3-edit-pagicconfigts-file-replace-pagic_template_docs-by-the-repository-name">3. Edit <code>pagic.config.ts</code> file, replace <code>pagic_template_docs</code> by the <em>Repository name</em><a class="anchor" href="#3-edit-pagicconfigts-file-replace-pagic_template_docs-by-the-repository-name">§</a></h2>\n<h2 id="4-go-to-settings--options--github-pages-and-set-branch-to-gh-pages">4. Go to <em>Settings</em> &gt; <em>Options</em> &gt; <em>GitHub Pages</em> and set Branch to gh-pages<a class="anchor" href="#4-go-to-settings--options--github-pages-and-set-branch-to-gh-pages">§</a></h2>'
@@ -26,7 +26,7 @@ export default {
         "YNlwys"
     ],
     'date': "2020-12-31T03:19:40.000Z",
-    'updated': "2021-01-06T03:16:34.000Z",
+    'updated': "2021-01-06T03:22:21.000Z",
     'excerpt': "1. Click the Use this template button in this repo 2. Enter the Repository name and click the Create repository from template button 3. Edit pagic.config.ts file, replace pagic_template_docs by the Repository n...",
     'cover': undefined,
     'blog': {
