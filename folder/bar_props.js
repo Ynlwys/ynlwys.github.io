5a7274483448f9676653a9ab8eb64bf4f05e60e3
Ynlwys <Ynlwys@users.noreply.github.com>
2021-01-06 02:00:19 +0000

    deploy: 87c4e1505d1b0c7e01d209d5659ed5ea97f8e788

--- folder/bar_props.js
+++ folder/bar_props.js
@@ -8,8 +8,7 @@ export default {
     'content': React.createElement("article", { dangerouslySetInnerHTML: {
             __html: '<h1>Bar</h1>\n<p>The bar page.</p>'
         } }),
-    'head': React.createElement(React.Fragment, null,
-        React.createElement("link", { crossOrigin: "anonymous", href: "https://cdn.jsdelivr.net/npm/katex@0.12.0/dist/katex.min.css", integrity: "sha384-AfEj0r4/OFrOo5t7NnNe46zW/tFgW6x/bCJG8FqQCEo3+Aro6EYUG4+cU+KJWu/X", rel: "stylesheet" })),
+    'head': null,
     'script': React.createElement(React.Fragment, null,
         React.createElement("script", { src: "https://cdn.pagic.org/react@16.13.1/umd/react.production.min.js" }),
         React.createElement("script", { src: "https://cdn.pagic.org/react-dom@16.13.1/umd/react-dom.production.min.js" }),
