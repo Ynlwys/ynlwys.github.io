58c56b71c2a0e84184744ebd08119349840f463e
Ynlwys <Ynlwys@users.noreply.github.com>
2021-01-06 08:39:13 +0000

    deploy: cc5db8d4eaf3bc0798e3e076017f6192457499df

---
+++ archives/index_props.js
@@ -0,0 +1,38 @@
+import projectConfig from '/pagic.config.js';
+export default {
+    config: { "root": "/", ...projectConfig, branch: 'main' },
+    'pagePath': "archives/README.md",
+    'layoutPath': "_layout.tsx",
+    'outputPath': "archives/index.html",
+    'title': "Pagic template docs",
+    'content': React.createElement("article", { dangerouslySetInnerHTML: {
+            __html: '<h1>Pagic template docs</h1>\n<h2 id="1-click-the-use-this-template-button-in-this-repo">1. Click the <em>Use this template</em> button in <a href="https://github.com/xcatliu/pagic_template_docs">this repo</a><a class="anchor" href="#1-click-the-use-this-template-button-in-this-repo">§</a></h2>\n<h2 id="2-enter-the-repository-name-and-click-the-create-repository-from-template-button">2. Enter the <em>Repository name</em> and click the <em>Create repository from template</em> button<a class="anchor" href="#2-enter-the-repository-name-and-click-the-create-repository-from-template-button">§</a></h2>\n<h2 id="3-edit-pagicconfigts-file-replace-pagic_template_docs-by-the-repository-name">3. Edit <code>pagic.config.ts</code> file, replace <code>pagic_template_docs</code> by the <em>Repository name</em><a class="anchor" href="#3-edit-pagicconfigts-file-replace-pagic_template_docs-by-the-repository-name">§</a></h2>\n<h2 id="4-go-to-settings--options--github-pages-and-set-branch-to-gh-pages">4. Go to <em>Settings</em> &gt; <em>Options</em> &gt; <em>GitHub Pages</em> and set Branch to gh-pages<a class="anchor" href="#4-go-to-settings--options--github-pages-and-set-branch-to-gh-pages">§</a></h2>'
+        } }),
+    'head': null,
+    'script': React.createElement(React.Fragment, null,
+        React.createElement("script", { src: "https://cdn.pagic.org/react@16.13.1/umd/react.production.min.js" }),
+        React.createElement("script", { src: "https://cdn.pagic.org/react-dom@16.13.1/umd/react-dom.production.min.js" }),
+        React.createElement("script", { src: "/index.js", type: "module" })),
+    'contentTitle': React.createElement("h1", { key: "0" }, "Pagic template docs"),
+    'contentBody': React.createElement("article", { dangerouslySetInnerHTML: {
+            __html: '<h2 id="1-click-the-use-this-template-button-in-this-repo">1. Click the <em>Use this template</em> button in <a href="https://github.com/xcatliu/pagic_template_docs">this repo</a><a class="anchor" href="#1-click-the-use-this-template-button-in-this-repo">§</a></h2>\n<h2 id="2-enter-the-repository-name-and-click-the-create-repository-from-template-button">2. Enter the <em>Repository name</em> and click the <em>Create repository from template</em> button<a class="anchor" href="#2-enter-the-repository-name-and-click-the-create-repository-from-template-button">§</a></h2>\n<h2 id="3-edit-pagicconfigts-file-replace-pagic_template_docs-by-the-repository-name">3. Edit <code>pagic.config.ts</code> file, replace <code>pagic_template_docs</code> by the <em>Repository name</em><a class="anchor" href="#3-edit-pagicconfigts-file-replace-pagic_template_docs-by-the-repository-name">§</a></h2>\n<h2 id="4-go-to-settings--options--github-pages-and-set-branch-to-gh-pages">4. Go to <em>Settings</em> &gt; <em>Options</em> &gt; <em>GitHub Pages</em> and set Branch to gh-pages<a class="anchor" href="#4-go-to-settings--options--github-pages-and-set-branch-to-gh-pages">§</a></h2>'
+        } }),
+    'toc': React.createElement("aside", { dangerouslySetInnerHTML: {
+            __html: '<nav class="toc"><ol><li><a href="#1-click-the-use-this-template-button-in-this-repo">1. Click the Use this template button in this repo</a></li><li><a href="#2-enter-the-repository-name-and-click-the-create-repository-from-template-button">2. Enter the Repository name and click the Create repository from template button</a></li><li><a href="#3-edit-pagicconfigts-file-replace-pagic_template_docs-by-the-repository-name">3. Edit pagic.config.ts file, replace pagic_template_docs by the Repository name</a></li><li><a href="#4-go-to-settings--options--github-pages-and-set-branch-to-gh-pages">4. Go to Settings &gt; Options &gt; GitHub Pages and set Branch to gh-pages</a></li></ol></nav>'
+        } }),
+    'author': "Ynlwys",
+    'contributors': [
+        "Ynlwys",
+        "YNlwys"
+    ],
+    'date': "2020-12-31T03:19:40.000Z",
+    'updated': "2021-01-06T08:37:40.000Z",
+    'excerpt': "1. Click the Use this template button in this repo 2. Enter the Repository name and click the Create repository from template button 3. Edit pagic.config.ts file, replace pagic_template_docs by the Repository n...",
+    'cover': undefined,
+    'blog': {
+        "isPost": false,
+        "posts": [],
+        "categories": [],
+        "tags": []
+    }
+};
