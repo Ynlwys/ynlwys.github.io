f2dc101236987a3c382e865678a696ec65787a18
Ynlwys <Ynlwys@users.noreply.github.com>
2021-01-07 08:14:50 +0000

    deploy: eed6bafe92bbabb4bed2cab8e154e38e34cfa0da

--- about/index_props.js
+++ about/index_props.js
@@ -4,26 +4,27 @@ export default {
     'pagePath': "about/README.md",
     'layoutPath': "_layout.tsx",
     'outputPath': "about/index.html",
-    'title': "About",
+    'title': "关于",
     'content': React.createElement("article", { dangerouslySetInnerHTML: {
-            __html: '<h1>About</h1>\n<p>The about page.</p>'
+            __html: '<h1>关于</h1>\n<p>The about page.</p>'
         } }),
     'head': null,
     'script': React.createElement(React.Fragment, null,
         React.createElement("script", { src: "https://cdn.pagic.org/react@16.13.1/umd/react.production.min.js" }),
         React.createElement("script", { src: "https://cdn.pagic.org/react-dom@16.13.1/umd/react-dom.production.min.js" }),
         React.createElement("script", { src: "/index.js", type: "module" })),
-    'contentTitle': React.createElement("h1", { key: "0" }, "About"),
+    'contentTitle': React.createElement("h1", { key: "0" }, "\u5173\u4E8E"),
     'contentBody': React.createElement("article", { dangerouslySetInnerHTML: {
             __html: '<p>The about page.</p>'
         } }),
     'toc': null,
     'author': "Ynlwys",
     'contributors': [
-        "Ynlwys"
+        "Ynlwys",
+        "YNlwys"
     ],
     'date': "2020-12-31T03:19:40.000Z",
-    'updated': null,
+    'updated': "2021-01-07T07:56:12.000Z",
     'excerpt': "The about page.",
     'cover': undefined,
     'blog': {
